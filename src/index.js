9cf3c3f4cda0a46b7e80fead17d8048fb5be888d
hrshdp <harshdeepchandrakar9@gmail.com>
2025-04-17 10:16:32 +0530

    forgot to import app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
-import connectDB from "./db/index.js";
-
 dotenv.config({
     path: './env'
 })
 
+import {app} from "./app.js"
+import connectDB from "./db/index.js";
 
 connectDB()
 .then(() => {
