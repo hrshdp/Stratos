f7792da8a2acb2170a8f01e643bf40ccd3e6f56b
hrshdp <harshdeepchandrakar9@gmail.com>
2025-04-24 07:25:50 +0530

    added register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,77 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js"
+import { User } from "../models/user.model.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 
 const registerUser = asyncHandler( async (req, res) => {
-    res.status(200).json({
-        message: "ok"
+    // ask user for email, name, username, password.
+    // check if user has sent empty fields and format of email is ok.
+    // check if user already exists: email and username
+    // heck for images, check for avatar
+    // upload them to cloudinary
+    // check if avatar uploaded on cloudinary or not
+    // create user object - create entry in db.
+    // remove password and refresh token field from response
+    // check for user creation 
+    // return response 
+
+    const { username, email, fullName, password } = req.body
+
+    // if (fullName === "") {
+    //     throw new ApiError(400, "fullname is required")
+    // } can check one by one like this using multiple if statements below is a better version of this
+
+    if ([fullName, email, username, password].some((field) => field?.trim() === "")
+    ) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const existedUser = User.findOne({
+        $or: [{ username },{ email }]
     })
+    //what if username is taken but email is not then ?
+
+    if (existedUser) {
+        throw new ApiError(409, "username or email already exists")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is required")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const cooverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!avatar) {
+        throw new ApiError(400, "Avatar file is required")
+    }
+
+    const user = await User.create({
+        fullName,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if (!createdUser) {
+        throw new ApiError(500, "Something wnet wrong while registering the user")
+    }
+
+    return res.ststus(201).json(
+        new ApiResponse(200, createdUser, "User registered successfully")
+    )
+    
 } )
 
 
